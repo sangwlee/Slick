17e08bc01f3d13116dd08e62f215d4640694441a
sangwlee <sangw.lee02@gmail.com>
2017-06-29 13:14:50 -0400

    .

--- app/controllers/api/emoticons_controller.rb
+++ app/controllers/api/emoticons_controller.rb
@@ -1,4 +1,5 @@
 class Api::EmoticonsController < ApplicationController
+
   def create
   @emoticon = Emoticon.new(emoticon_params)
 
@@ -8,37 +9,37 @@ class Api::EmoticonsController < ApplicationController
   else
     render json: @emoticon.errors.full_messages, status: 422
   end
-end
 
-def update
-  @emoticon = Emoticon.find(params[:id])
+  def update
+    @emoticon = Emoticon.find(params[:id])
 
-  if @emoticon.update_attributes(emoticon_params)
-    render :show
-  else
-    render json: @emoticon.errors.full_messages, status: 422
+    if @emoticon.update_attributes(emoticon_params)
+      render :show
+    else
+      render json: @emoticon.errors.full_messages, status: 422
+    end
   end
-end
 
-def destroy
-  @emoticon = Emoticon.find(params[:id])
-  
-  if @emoticon
-    @emoticon.destroy
-    render json: @emoticon.id
-  else
-    render(
-      json: ["Emoticon not found"],
-      status: 404
-      )
+  def destroy
+    @emoticon = Emoticon.find(params[:id])
+
+    if @emoticon
+      @emoticon.destroy
+      render json: @emoticon.id
+    else
+      render(
+        json: ["Emoticon not found"],
+        status: 404
+        )
+    end
   end
-end
 
-def show
-  message = Message.find_by(id: params[:id]).includes(:emoticons)
-  @emoticons = message.emoticons
-end
+  def show
+    message = Message.find_by(id: params[:id]).includes(:emoticons)
+    @emoticons = message.emoticons
+  end
 
-def emoticon_params
-  params.require(:emoticon).permit(:user_id, :message_id, :icon)
+  def emoticon_params
+    params.require(:emoticon).permit(:user_id, :message_id, :icon)
+  end
 end
